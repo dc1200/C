8f735b2dafeca611b0793b64f888261e3737b827
dc1200 <9923103306@mail.jiit.ac.in>
2025-07-29 11:28:28 +0530

    Update calculator.c
    Whitespace before %c ensures the input buffer is cleared.
    
    Division by zero check adds safety.
    
    More readable formatting of scanf calls and switch statement.

--- calculator.c
+++ calculator.c
@@ -1,32 +1,31 @@
-//C program to display simple calculator.
-
 #include<stdio.h>
 #include<math.h>
+
 int main()
 {
-  float n1,n2;              //Declaring two float varibles, which we will later use as two operands.
-  char operator;            //The operator is a char varible which will store our choice of operator. ex. +, -, *, /
-  printf("Enter an operator(+,-,/,*,^):");
-  scanf("%c",&operator);    //Taking input from user as what operation they want to perform.
+  float n1, n2;
+  char operator;
 
-  printf("Enter two numbers:");
-  scanf("%f%f",&n1,&n2);    //Taking float inputs as operands and storing then in n1 and n2 respectively
-  
-  switch(operator)          //Here depending upon the operator input switch statement will decide which case to execute
+  printf("Enter an operator (+, -, /, *, ^): ");
+  scanf(" %c", &operator);  // Add a space before %c to consume any leftover whitespace
+
+  printf("Enter two numbers: ");
+  scanf("%f %f", &n1, &n2);
+
+  switch(operator)
   {
-      //The %.3f will allow only three values after the dot(.) to print in the float
-    case'+' : printf("%.3f + %.3f = %.3f",n1,n2,(n1+n2)); 
-              break;
-    case'-' : printf("%.3f - %.3f = %.3f",n1,n2,(n1-n2));
-              break;
-    case'/' : printf("%.3f / %.3f = %.3f",n1,n2,(n1/n2));
-              break;
-    case'*' : printf("%.3f * %.3f = %.3f",n1,n2,(n1*n2));
-              break;
-    case'^' : printf("%.3f ^ %.3f = %.3f",n1,n2,pow(n1, n2));
-              break;
-    default: printf("Error! operator is wrong");
-
+    case '+' : printf("%.3f + %.3f = %.3f", n1, n2, n1 + n2); break;
+    case '-' : printf("%.3f - %.3f = %.3f", n1, n2, n1 - n2); break;
+    case '/' : 
+      if(n2 != 0)
+        printf("%.3f / %.3f = %.3f", n1, n2, n1 / n2);
+      else
+        printf("Error! Division by zero");
+      break;
+    case '*' : printf("%.3f * %.3f = %.3f", n1, n2, n1 * n2); break;
+    case '^' : printf("%.3f ^ %.3f = %.3f", n1, n2, pow(n1, n2)); break;
+    default  : printf("Error! Operator is not supported");
   }
+
   return 0;
 }
